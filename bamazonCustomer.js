ecc048346a2771390b5d8533c9908bd68f178c84
enigmatic-agent-scully <amanda.elisabet.jordan@gmail.com>
2018-11-02 21:33:44 -0400

    first few steps done!

---
+++ bamazonCustomer.js
@@ -0,0 +1,63 @@
+var mysql = require('mysql');
+var inquirer = require('inquirer');
+
+var connection = mysql.createConnection({
+  host: 'localhost',
+  port: 3306,
+  user: 'root',
+  database: 'bamazon'
+});
+
+
+
+function welcome() {
+  inquirer.prompt({
+    type: 'rawlist',
+    name: 'shop',
+    message: 'Welcome to BAMAZON! Would you like to [SHOP DEPARTMENTS] or [SHOP ALL PRODUCTS]?',
+    choices: ['SHOP DEPARTMENTS', 'SHOP ALL PRODUCTS']
+  }).then(answer => {
+    if (answer.shop === 'SHOP DEPARTMENTS') {
+      shopDepts();
+    }
+    else {
+      //shopAll;
+    }
+  })
+}
+
+function shopDepts() {
+  connection.query('SELECT dept FROM products', function (err, resp) {
+    if (err) console.log(err);
+    inquirer.prompt({
+      type: 'rawlist',
+      name: 'depts',
+      message: 'which department would you like to browse?',
+      choices: function () {
+        var deptsArr = [];
+        for (var i = 0; i < resp.length; i++) {
+          if (!deptsArr.includes(resp[i].dept)) {
+            deptsArr.push(resp[i].dept);
+          }
+        }
+        return deptsArr;
+      }
+    })
+      .then(ans => {
+        switch (ans.depts) {
+          case 'electronics':
+            connection.query(`SELECT itemname FROM products WHERE ?`, {dept: ans.depts}, function (err, resp) {
+              console.log(resp.itemname);
+            }
+            )
+        }
+      }
+      )
+      .catch((err) => {
+        console.log(err);
+      })
+  }
+  )
+}
+
+welcome();
