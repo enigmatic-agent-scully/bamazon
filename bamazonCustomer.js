977095519c5c03d8e0c150c6d9224f139dcf3689
AJ <amanda.elisabet.jordan@gmail.com>
2018-12-27 17:46:24 -0500

    manager portal works!

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -8,152 +8,190 @@ var connection = mysql.createConnection({
   database: 'bamazon'
 });
 
-
 const welcome = () => {
-  inquirer.prompt({
-    type: 'rawlist',
-    name: 'shop',
-    message: 'Welcome to BAMAZON! Would you like to SHOP DEPARTMENTS or SHOP ALL PRODUCTS?',
-    choices: ['SHOP DEPARTMENTS', 'SHOP ALL PRODUCTS']
-  }).then(answer => {
-    if (answer.shop === 'SHOP DEPARTMENTS') {
-      shopDepts();
-    }
-    else {
-      shopAll();
-    }
-  })
-}
+  inquirer
+    .prompt({
+      type: 'rawlist',
+      name: 'shop',
+      message:
+        'Welcome to BAMAZON! Would you like to SHOP DEPARTMENTS or SHOP ALL PRODUCTS?',
+      choices: ['SHOP DEPARTMENTS', 'SHOP ALL PRODUCTS']
+    })
+    .then(answer => {
+      if (answer.shop === 'SHOP DEPARTMENTS') {
+        shopDepts();
+      } else {
+        shopAll();
+      }
+    });
+};
 
 welcome();
 
 const shopDepts = () => {
-  connection.query('SELECT dept FROM products', function (err, resp) {
+  connection.query('SELECT dept FROM products', (err, resp) => {
     if (err) console.log(err);
-    inquirer.prompt({
-      type: 'rawlist',
-      name: 'depts',
-      message: 'Which department would you like to browse?',
-      choices: () => {
-        var deptsArr = [];
-        for (var i = 0; i < resp.length; i++) {
-          if (!deptsArr.includes(resp[i].dept)) {
-            deptsArr.push(resp[i].dept);
-          }
-        }
-        return deptsArr;
-      }
-    })
-      .then(ans => {
-        connection.query(`SELECT id, itemname, price, quant FROM products WHERE ?`, { dept: ans.depts }, (err, resp) => {
-          if (err) throw err;
-          else {
-            console.log(`You are now browsing ALL available products in the ${ans.depts} department.\n`);
-            for (var i = 0; i < resp.length; i++) {
-              console.log(`${resp[i].id} - ${resp[i].itemname} || price: $${resp[i].price}.00 || quantity: ${resp[i].quant}`);
+    inquirer
+      .prompt({
+        type: 'rawlist',
+        name: 'depts',
+        message: 'Which department would you like to browse?',
+        choices: () => {
+          var deptsArr = [];
+          for (var i = 0; i < resp.length; i++) {
+            if (!deptsArr.includes(resp[i].dept)) {
+              deptsArr.push(resp[i].dept);
             }
-            inquirer.prompt([{
-              type: 'input',
-              name: 'itemid',
-              message: 'What is the ID of the item you would like to purchase?',
-              validate: (resp) => {
-                if (isNaN(resp)) {
-                  return false;
-                }
-                return true;
-              }
-            },
-            {
-              type: 'input',
-              name: 'quantity',
-              message: 'How many units of this item would you like to purchase?',
-              validate: (resp) => {
-                if (isNaN(resp)) {
-                  return false;
-                }
-                return true;
-              }
-            }]).then(ans => {
-              checkQuant(ans.itemid, ans.quantity);
-            })
           }
-        })
-      }
-      )
-  })
+          return deptsArr;
+        }
+      })
+      .then(ans => {
+        connection.query(
+          `SELECT id, itemname, price, quant FROM products WHERE ?`,
+          { dept: ans.depts },
+          (err, resp) => {
+            if (err) throw err;
+            else {
+              console.log(
+                `You are now browsing ALL available products in the ${
+                  ans.depts
+                } department.\n`
+              );
+              for (var i = 0; i < resp.length; i++) {
+                console.log(
+                  `${resp[i].id} - ${resp[i].itemname} || price: $${
+                    resp[i].price
+                  }.00 || quantity: ${resp[i].quant}`
+                );
+              }
+              inquirer
+                .prompt([
+                  {
+                    type: 'input',
+                    name: 'itemid',
+                    message:
+                      'What is the ID of the item you would like to purchase?',
+                    validate: resp => {
+                      if (isNaN(resp)) {
+                        return false;
+                      }
+                      return true;
+                    }
+                  },
+                  {
+                    type: 'input',
+                    name: 'quantity',
+                    message:
+                      'How many units of this item would you like to purchase?',
+                    validate: resp => {
+                      if (isNaN(resp)) {
+                        return false;
+                      }
+                      return true;
+                    }
+                  }
+                ])
+                .then(ans => {
+                  checkQuant(ans.itemid, ans.quantity);
+                });
+            }
+          }
+        );
+      });
+  });
 };
 
 const shopAll = () => {
-  connection.query('SELECT * FROM products', function (err, resp) {
+  connection.query('SELECT * FROM products', function(err, resp) {
     console.log('You are now browsing ALL available products.\n');
     for (var i = 0; i < resp.length; i++) {
-      console.log(`${resp[i].id} - ${resp[i].itemname} || price: $${resp[i].price}.00 || quantity: ${resp[i].quant}`);
-    };
-    inquirer.prompt([{
-      type: 'input',
-      name: 'itemid',
-      message: 'What is the ID of the item you would like to purchase?',
-      validate: function (resp) {
-        if (isNaN(resp)) {
-          return false;
+      console.log(
+        `${resp[i].id} - ${resp[i].itemname} || price: $${
+          resp[i].price
+        }.00 || quantity: ${resp[i].quant}`
+      );
+    }
+    inquirer
+      .prompt([
+        {
+          type: 'input',
+          name: 'itemid',
+          message: 'What is the ID of the item you would like to purchase?',
+          validate: function(resp) {
+            if (isNaN(resp)) {
+              return false;
+            }
+            return true;
+          }
+        },
+        {
+          type: 'input',
+          name: 'quantity',
+          message: 'How many units of this item would you like to purchase?',
+          validate: function(resp) {
+            if (isNaN(resp)) {
+              return false;
+            }
+            return true;
+          }
         }
-        return true;
-      }
-    },
-    {
-      type: 'input',
-      name: 'quantity',
-      message: 'How many units of this item would you like to purchase?',
-      validate: function (resp) {
-        if (isNaN(resp)) {
-          return false;
-        }
-        return true;
-      }
-    }]).then(ans => {
-      checkQuant(ans.itemid, ans.quantity);
-    })
-  })
-}
-
-
+      ])
+      .then(ans => {
+        checkQuant(ans.itemid, ans.quantity);
+      });
+  });
+};
 
 function checkQuant(id, q) {
-  connection.query('SELECT itemname, quant FROM products WHERE ?', { id: id }, (err, resp) => {
-
-    var storeQ = resp[0].quant;
-    if (q <= storeQ) {
-      makePurchase(id, q, storeQ);
+  connection.query(
+    'SELECT itemname, quant FROM products WHERE ?',
+    { id: id },
+    (err, resp) => {
+      var storeQ = resp[0].quant;
+      if (q <= storeQ) {
+        makePurchase(id, q, storeQ);
+      } else {
+        console.log(q, storeQ, 'Insufficient quantity!');
+      }
     }
-    else {
-      console.log(q, storeQ, 'Insufficient quantity!');
-    }
-  })
+  );
 }
 
 const makePurchase = (x, y, z) => {
-  var newQ = (z - y);
+  var newQ = z - y;
 
-  connection.query('UPDATE products SET ? WHERE ?', [
-    {
-      quant: newQ
-    },
-    {
-      id: x
-    }
-  ], (err, resp) => {
-    if (err) throw err;
-    else {
-      console.log('Your order has been placed!\n\nThank you for shopping at BAMAZON!');
-      welcome();
-
-    }
-  });
-  if (newQ === 0) {
-    connection.query('DELETE FROM products WHERE ?', [{
-      quant: 0
-    }], (err, resp) => {
+  connection.query(
+    'UPDATE products SET ? WHERE ?',
+    [
+      {
+        quant: newQ
+      },
+      {
+        id: x
+      }
+    ],
+    (err, resp) => {
       if (err) throw err;
-    })
+      else {
+        console.log(
+          'Your order has been placed!\n\nThank you for shopping at BAMAZON!'
+        );
+        welcome();
+      }
+    }
+  );
+  if (newQ === 0) {
+    connection.query(
+      'DELETE FROM products WHERE ?',
+      [
+        {
+          quant: 0
+        }
+      ],
+      (err, resp) => {
+        if (err) throw err;
+      }
+    );
   }
-}
+};
--- bamazonManager.js
+++ bamazonManager.js
@@ -9,92 +9,195 @@ var connection = mysql.createConnection({
 });
 
 const menu = () => {
-  inquirer.prompt({
-    type: 'rawlist',
-    name: 'menu',
-    message: 'Hello Bamazon Manager. Welcome to your Manager Portal. Please choose from the following options:',
-    choices: ['View Inventory', 'View Low Inventory', 'Add to Inventory', 'Add New Product']
-  })
+  inquirer
+    .prompt({
+      type: 'rawlist',
+      name: 'menu',
+      message:
+        'Hello Bamazon Manager. Welcome to your Manager Portal. Please choose from the following options:',
+      choices: [
+        'View Inventory',
+        'View Low Inventory',
+        'Add to Inventory',
+        'Add New Product'
+      ]
+    })
     .then(ans => {
       if (ans.menu === 'View Inventory') {
         viewAll();
-      }
-      else if (ans.menu === 'View Low Inventory') {
+      } else if (ans.menu === 'View Low Inventory') {
         viewLow();
-      }
-      else if (ans.menu === 'Add to Inventory') {
+      } else if (ans.menu === 'Add to Inventory') {
         addStock();
+      } else if (ans.menu === 'Add New Product') {
+        addNew();
       }
-      else if (ans.menu === 'Add New Product') {
-        //addNew();
-      }
-    })
-}
+    });
+};
 
 const viewAll = () => {
-  connection.query('SELECT * FROM products', function (err, resp) {
+  connection.query('SELECT * FROM products', function(err, resp) {
     console.log('You are now browsing ALL available products.\n');
     for (var i = 0; i < resp.length; i++) {
-      console.log(`${resp[i].id} - ${resp[i].itemname} || price: $${resp[i].price}.00 || quantity: ${resp[i].quant}`);
+      console.log(
+        `${resp[i].id} - ${resp[i].itemname} || price: $${
+          resp[i].price
+        }.00 || quantity: ${resp[i].quant}`
+      );
     }
-  })
-}
+  });
+};
 
 const viewLow = () => {
-  connection.query('SELECT * FROM products', function (err, resp) {
+  connection.query('SELECT * FROM products', function(err, resp) {
     console.log('You are now browsing LOW inventory.\n');
     var count = [];
     for (var i = 0; i < resp.length; i++) {
       if (resp[i].quant <= 5) {
-        console.log(`${resp[i].id} - ${resp[i].itemname} || price: $${resp[i].price}.00 || quantity: ${resp[i].quant}`);
+        console.log(
+          `${resp[i].id} - ${resp[i].itemname} || price: $${
+            resp[i].price
+          }.00 || quantity: ${resp[i].quant}`
+        );
         count.push(resp[i].id);
       }
     }
     if (count.length === 0) {
-      console.log('You have no low inventory items at this time.')
+      console.log('You have no low inventory items at this time.');
     }
-  })
-}
+  });
+};
 
 const addStock = () => {
-  console.log('Welcome to the Add Stock Wizard. The Add Stock Wizard will walk you through adding stock to your inventory.');
+  console.log(
+    'Welcome to the Add Stock Wizard. The Add Stock Wizard will walk you through adding stock to your inventory.'
+  );
   connection.query('SELECT id, itemname, quant FROM products', (err, resp) => {
     if (err) throw err;
     for (var i = 0; i < resp.length; i++) {
-      console.log(`${resp[i].id} - ${resp[i].itemname} || quantity: ${resp[i].quant}`);
-    };
-    inquirer.prompt([{
-      type: 'input',
-      name: 'id',
-      message: `What is the ID of the item to which you'd like to add stock?`,
-      validate: function (resp) {
-        if (isNaN(resp)) {
-          return false;
-        }
-        return true;
-      }
-    },
-    {
-      type: 'input',
-      name: 'addQ',
-      message: 'How many units of this item would you like to add to your inventory?',
-      validate: function (resp) {
-        if (isNaN(resp)) {
-          return false;
-        }
-        return true;
-      }
-    }]).then(ans => {
-      connection.query('UPDATE products SET ? WHERE ?', {
-        quantity: ans.addQ
-      },
-        {
-          id: ans.id
-        })
+      console.log(
+        `${resp[i].id} - ${resp[i].itemname} || quantity: ${resp[i].quant}`
+      );
     }
-    )
-  })
+    inquirer
+      .prompt([
+        {
+          type: 'input',
+          name: 'id',
+          message: `What is the ID of the item to which you'd like to add stock?`,
+          validate: resp => {
+            if (isNaN(resp)) {
+              return false;
+            }
+            return true;
+          }
+        },
+        {
+          type: 'input',
+          name: 'addQ',
+          message:
+            'How many units of this item would you like to add to your inventory?',
+          validate: function(resp) {
+            if (isNaN(resp)) {
+              return false;
+            }
+            return true;
+          }
+        }
+      ])
+      .then(ans => {
+        var sql = 'UPDATE products SET ?? = ? WHERE ?? = ?';
+        var inserts = ['quant', ans.addQ, 'id', ans.id];
+        sql = mysql.format(sql, inserts);
+        console.log(sql);
 
-}
+        connection.query(sql, (err, resp) => {
+          if (err) throw err;
+          console.log('Stock added!');
+        });
+        menu();
+      });
+  });
+};
+
+const addNew = () => {
+  console.log(
+    'Welcome to the Add New Inventory Wizard. The Add New Inventory Wizard will walk you through adding new items to your inventory.'
+  );
+  connection.query('SELECT dept FROM products', (err, resp) => {
+    if (err) throw err;
+    else {
+      inquirer
+        .prompt([
+          {
+            type: 'input',
+            name: 'newItem',
+            message: `What is the name of the new item you'd like to add to the inventory?`,
+            validate: res => {
+              if (!isNaN(res)) {
+                return false;
+              }
+              return true;
+            }
+          },
+          {
+            type: 'rawlist',
+            name: 'dept',
+            message: 'Into which department should this new item be added?',
+            choices: () => {
+              var deptsArr = [];
+              for (var i = 0; i < resp.length; i++) {
+                if (!deptsArr.includes(resp[i].dept)) {
+                  deptsArr.push(resp[i].dept);
+                }
+              }
+              return deptsArr;
+            }
+          },
+          {
+            type: 'input',
+            name: 'price',
+            message: `What is the item's list price?`,
+            validate: res => {
+              if (isNaN(res)) {
+                return false;
+              }
+              return true;
+            }
+          },
+          {
+            type: 'input',
+            name: 'quant',
+            message:
+              'How many units of this item would you like to add to your inventory?',
+            validate: res => {
+              if (isNaN(res)) {
+                return false;
+              }
+              return true;
+            }
+          }
+        ])
+        .then(ans => {
+          var sql = `INSERT INTO products (??, ??, ??, ??) VALUES (?, ?, ?, ?)`;
+          var inserts = [
+            'itemname',
+            'dept',
+            'price',
+            'quant',
+            ans.newItem,
+            ans.dept,
+            ans.price,
+            ans.quant
+          ];
+          sql = mysql.format(sql, inserts);
+          connection.query(sql, (err, resp) => {
+            if (err) throw err;
+            console.log('New inventory item added!');
+          });
+        });
+    }
+  });
+};
 
 menu();
